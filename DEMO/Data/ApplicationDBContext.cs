35a25a8bd3c641b33479e6c856e5a0cac36802a1
Gene Harold <Gene Harold@GENE>
2024-09-08 17:55:33 +0800

    Add project files.

---
+++ DEMO/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using DEMO.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEMO.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options): base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
